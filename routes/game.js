efe08bf5cc2bc0a9895b614dfaee0fb9add85f3a
Jonathan Ming <jming@pkcsecurity.com>
2018-12-11 12:49:44 -0800

    Add GET /gameName route for getting game state

--- routes/game.js
+++ routes/game.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const uuid = require("uuid/v4");
 const dynamo = require("../dynamodb");
 
 /*
@@ -20,16 +21,27 @@ router.get("/", (req, res) => {
  *   ownerEmail: "",
  *   maxScore: 0
  * }
+ * Responds with the following body:
+ * { token: "" }
  */
 router.post("/", (req, res) => {
+  const token = uuid();
   dynamo.initNewGame(req.gameName, token, req.ownerEmail, req.maxScore);
+  res.send({ token: token });
 });
 
 // POST a new player enters a game
 router.post("/:gameId/player");
 
 // GET game state
-router.get("/:gameId");
+router.get("/:gameName", (req, res) => {
+  const game = dynamo.getGameState(req.params.gameName);
+  if (game) {
+    res.send(game.GameState);
+  } else {
+    res.status(404).end();
+  }
+});
 
 // POST round leader sets the word for the round
 router.post("/:gameId/word");
