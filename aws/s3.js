d7ff239675a37d719b2043367b62d6f779a2d7dd
Jonathan Ming <jming@pkcsecurity.com>
2018-12-11 17:49:28 -0800

    Fix

--- aws/s3.js
+++ aws/s3.js
@@ -1,27 +1,26 @@
-const AWS = require('aws-sdk');
-AWS.config.update({region: "us-west-2"});
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-west-2" });
 const s3 = new AWS.S3();
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
 
 const uploadImage = async (bucketName, imgName, img) => {
-  const params = {Bucket: bucketName, Key: imgName, Body: img};
-  return await s3.putObject().promise();
-}
+  const params = { Bucket: bucketName, Key: imgName, Body: img };
+  return await s3.putObject(params).promise();
+};
 
 const getImage = async (bucketName, imgName) => {
-  const params = {Bucket: bucketName, Key: imgName};
+  const params = { Bucket: bucketName, Key: imgName };
   return await s3.getObject(params).promise();
-}
+};
 
-const getImgsOfBucket = async (bucketName) => {
-  const params = {Bucket: bucketName};
+const getImgsOfBucket = async bucketName => {
+  const params = { Bucket: bucketName };
   return await s3.listObjects(params).promise();
-}
+};
 
 module.exports = {
   uploadImage,
   getImage,
   getImgsOfBucket
-}
-
+};
