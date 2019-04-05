f7e3a4eecfb974ab3304217740f71b1e643c111b
a5834929 <a5834929@gmail.com>
2019-04-05 16:35:45 -0700

    create new game page

--- app/lib/view/newGamePage.dart
+++ app/lib/view/newGamePage.dart
@@ -1,16 +1,82 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:app/view/widgets/color.dart';
 
 class NewGamePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: Center(
-        child: FlatButton(
-          onPressed: () =>
-              Navigator.of(context).popUntil((Route r) => r.settings.name == '/home'),
-          child: Text("New Game")
+    final mediaData = MediaQuery.of(context);
+    final height = mediaData.size.height;
+    final width = mediaData.size.width;
+
+    return _background(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
         ),
-      ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: height*0.2,
+            ),
+            Text(
+              "Create a new game\nand play with your friends!",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20.0
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Enter Your Game Name",
+                  hintText: "My Awesome Game",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24.0),
+                    ),
+                  ),
+                ),
+              ),
+            _submitButton(context),
+            ],
+          )
+        )
+    );
+  }
+
+  Widget _background({Widget child}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: LightBlue,
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/backgrounds/3.0x/bgCirclesSplash.png'),
+          fit: BoxFit.cover,
+          )
+        ),
+      child: child,
+    );
+  }
+
+  Widget _submitButton(BuildContext context) {
+    return FlatButton(
+      onPressed: () => Navigator.of(context).popUntil((Route r) => r.settings.name == '/home'),
+      child: Text("Let's Play!")
+    );
+  }
+
+  Widget _textField() {
+    return TextField(
+
     );
   }
 }
+
--- app/lib/view/widgets/color.dart
+++ app/lib/view/widgets/color.dart
@@ -4,3 +4,4 @@ const DarkBackground = const Color.fromRGBO(53, 55, 51, 1.0);
 const LightOliveGreen = const Color(0xffabcb46);
 const DarkOliveGreen = const Color(0xff9fbc42);
 const RedBackground = const Color(0xffd03b28);
+const LightBlue = const Color(0xffa9cef4);
